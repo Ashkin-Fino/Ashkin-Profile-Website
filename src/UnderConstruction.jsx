b0f137b69430859d23afe24561808cbb1629ba98
Ashkin Fino <rashkinfino421@gmail.com>
2025-09-13 09:40:26 +0530

    Added place holder for under contruction pages

---
+++ src/UnderConstruction.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./UnderConstruction.css";
+
+function UnderConstruction() {
+    
+    return (
+        <section className="under-construction">
+        <div className="construction-content">
+            🚧 This section is under development 🚧
+        </div>
+    </section>
+    );
+}
+
+export default UnderConstruction;
